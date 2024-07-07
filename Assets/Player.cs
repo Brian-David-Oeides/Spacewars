62777421f2235e59985ed3907ba1714954d1696a
Brian-David-Oeides <bdgaisi@gmail.com>
2024-07-07 20:48:00 +0700

    Constrained player movement in Game view and optimized code.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,22 +7,32 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed = 3.5f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        //Set the current position of this game object to = new position (0,0,0)
         transform.position = new Vector3(0, 0, 0);
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        CalculateMovement();
+    }
+    void CalculateMovement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        // new Vector3 (1,0,0) * 0 * 3.5f * real-time
-        // transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        // transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
         Vector3 direction = new Vector3(horizontalInput, verticalInput,0);
-        transform.Translate(direction * _speed * Time.deltaTime)
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        if (transform.position.x > 11.29f)
+        {
+            transform.position = new Vector3(-11.29f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.29f)
+        {
+            transform.position = new Vector3(11.29f, transform.position.y, 0);
+        }
     }
 }
