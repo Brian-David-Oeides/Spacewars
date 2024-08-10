faf8e48eb0baee7358fc16f13709303a1a7eceb7
Brian David <164612324+Brian-David-Oeides@users.noreply.github.com>
2024-08-10 13:05:02 +0700

    dded asteroid prefab, created explosion prefab, added VFX to asteroid, added script for asteroid and explosion behavior

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    // rotation speed
+    private float _rotateSpeed = 18.0f;
+
+    [SerializeField]
+    private GameObject _explosionPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // rotate this game object on z-axis
+        transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+    }
+
+    // Check for laser collision (Trigger)
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject, 0.25f);
+        }
+    }
+    // Instantiate explosion at the position of the asteroid
+    // Destroy the explosion after 3 seconds.
+}
